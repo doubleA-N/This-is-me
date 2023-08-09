72356bfe68565578df7bdb184667c196bceaf9a9
Aey-Thidarat <thidarat.c@pomelofashion.com>
2023-08-09 20:11:45 +0700

    feat(navigation): implement desktop navigation

---
+++ src/app/components/navigation/const.js
@@ -0,0 +1,26 @@
+import { ICONS } from '../commons/icons/const'
+
+export const NAVIGATION_MENUS = [
+  { title: 'Home', shouldShowNextMenu: true },
+  { title: 'About', shouldShowNextMenu: true },
+  { title: 'Skills', shouldShowNextMenu: true },
+  { title: 'Contact Me', shouldShowNextMenu: false },
+]
+
+export const NAVIGATION_LINKS = [
+  {
+    src: ICONS.linkedIn,
+    alt: 'linkedIn',
+    link: 'https://www.linkedin.com/in/thidarat-chaichana-32968720a',
+  },
+  {
+    src: ICONS.facebook,
+    alt: 'facebook',
+    link: 'https://www.facebook.com/profile.php?id=100009325695045',
+  },
+  {
+    src: ICONS.github,
+    alt: 'github',
+    link: 'https://github.com/doubleA-N/This-is-me',
+  },
+]
---
+++ src/app/components/navigation/desktop/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import { ICONS, ICON_SIZE } from '../../commons/icons/const'
+import DesktopSeparateIcon from '../../commons/icons/desktopSeparateIcon'
+import Icon from '../../commons/icons'
+import { NAVIGATION_LINKS, NAVIGATION_MENUS } from '../const'
+
+const DesktopNavigation = () => {
+  const [isMenuActive, setIsMenuActive] = useState(0)
+
+  const handleSocialLinkClick = (link) => {
+    window.open(link, '_blank').focus()
+  }
+
+  return (
+    <React.Fragment>
+      <Icon
+        className="text-3xl font-bold leading-none"
+        onClick={() => setIsMenuActive(0)}
+        src={ICONS.thisIsMe}
+        size={ICON_SIZE.extraLarge}
+        alt="Picture of the author"
+      />
+      <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
+        {NAVIGATION_MENUS.map((menu, idx) => (
+          <div
+            key={idx}
+            className="lg:flex lg:items-center gap-4 cursor-pointer"
+          >
+            <li>
+              <span
+                className={clsx(
+                  'text-sm text-gray-400 hover:text-violet-700 hover:font-bold',
+                  {
+                    'text-violet-700 font-bold': isMenuActive === idx,
+                  },
+                )}
+                onClickCapture={() => setIsMenuActive(idx)}
+              >
+                {menu.title}
+              </span>
+            </li>
+            {menu.shouldShowNextMenu && <DesktopSeparateIcon />}
+          </div>
+        ))}
+      </ul>
+      <div className="lg:flex lg:items-center gap-2 cursor-pointer">
+        {NAVIGATION_LINKS.map((icon) => (
+          <Icon
+            src={icon.src}
+            size={ICON_SIZE.medium}
+            alt={icon.alt}
+            onClick={() => handleSocialLinkClick(icon.link)}
+          />
+        ))}
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default DesktopNavigation
