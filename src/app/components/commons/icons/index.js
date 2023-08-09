69ca7ec428a40307dc389700be04397eab556292
Aey-Thidarat <thidarat.c@pomelofashion.com>
2023-08-09 20:10:56 +0700

    feat(navigation): implement icon common components

---
+++ src/app/components/commons/icons/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Image from 'next/image'
+import { ICON_SIZE } from './const'
+
+const Icon = ({ alt, className, src, size, onClick }) => {
+  return (
+    <div className={className || ''}>
+      <Image
+        alt={alt}
+        src={src}
+        width={size || ICON_SIZE.default}
+        height={size || ICON_SIZE.default}
+        onClick={onClick}
+      />
+    </div>
+  )
+}
+
+Icon.propTypes = {
+  alt: PropTypes.string,
+  className: PropTypes.string,
+  src: PropTypes.string,
+  size: PropTypes.number,
+  onClick: PropTypes.func,
+}
+
+export default Icon
