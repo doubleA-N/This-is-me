7ee3155bb0fa74af9861796d708fcec3dca8692a
Aey-Thidarat <thidarat.c@pomelofashion.com>
2023-08-19 18:41:34 +0700

    chore(scroll-lib): clean code

--- src/app/components/me-section/utils.js
+++ src/app/components/me-section/utils.js
@@ -1,5 +1,4 @@
 export const downloadMyResume = () => {
-    console.log('click', window)
   if (window !== 'undefined') {
     window.open(
       'https://drive.google.com/file/d/1uCfLDrbhK5WnYVw10vVgBPSVEaekS8mi/view?usp=sharing',
