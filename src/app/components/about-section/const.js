d6be3b2deeaad7a106c6b5e75bb70e0e977cacf7
Aey-Thidarat <thidarat.c@pomelofashion.com>
2023-08-20 23:59:35 +0700

    feat(about-section): improve info and components

--- src/app/components/about-section/const.js
+++ src/app/components/about-section/const.js
@@ -1,19 +1,23 @@
 export const MY_ABOUT = [
   {
-    title: 'My Graduation',
-    sub_title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    title: 'My Education',
+    sub_title: `I am proud to have pursued Computer Engineering at King Mongkut's University of Technology Thonburi (KMUTT) and gained invaluable experience through my various term projects and extracurricular activities. The 4 years I spent there were not only enjoyable but also enriched my life in meaningful ways.`,
+    timeline: '06/2017 - 06/2021 Bangkok, Thailand',
   },
   {
-    title: 'Internship',
-    sub_title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    title: 'Internship - System Analysis at True Corporation',
+    sub_title: `I thoroughly examined the existing systems and developed some innovative ideas to enhance the website's functionality. Additionally, I designed a user-friendly dashboard that simplifies data analysis. Lastly, I created comprehensive guides and documentation to ensure a seamless process for everyone involved.`,
+    timeline: '06/2020 - 07/2020 Bangkok, Thailand',
   },
   {
     title: 'Pomelo - Associate Software Engineer',
-    sub_title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    sub_title: `I was dedicated to enhancing the website's performance and functionality by identifying and resolving bottlenecks. My efforts included developing features such as a voucher system and product detail components. I also implemented event-tracking mechanisms to gain insights into user behavior. My contributions to code reviews were focused on maintaining code quality and adherence to coding standards through constructive feedback. My ultimate aim was to provide users with a seamless browsing experience.`,
+    timeline: '07/2021 - 02/2022 Bangkok, Thailand',
   },
   {
     title: 'Pomelo - Senior Software Engineer',
-    sub_title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    sub_title: `As a key team member, I led the way in developing crucial platform features. This included a loyalty program that boosted customer engagement and retention, as well as integrating multiple payment gateways to expand the platform's reach. To gain valuable insights into user behavior, I implemented event-tracking mechanisms. Additionally, I utilized cutting-edge technologies to create efficient and high-performing frontend code, ensuring optimal website speed and responsiveness. Throughout this process, I worked closely with cross-functional teams to ensure that our frontend solutions aligned with business objectives and user needs.`,
+    timeline: '02/2022 - Present Bangkok, Thailand',
   },
 ]
 
--- src/app/components/about-section/index.js
+++ src/app/components/about-section/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Element } from 'react-scroll'
-import { MY_ABOUT } from './const'
+import MyTimeline from './timeline'
 
 const AboutSection = ({}) => {
   return (
@@ -11,52 +11,7 @@ const AboutSection = ({}) => {
       <div className="flex flex-col items-center">
         <span className="text-violet-700 text-5xl font-bold">About</span>
         <span className="text-gray-700 text-l">Here's My Experience</span>
-        
-        <div className="sm:z-0 relative wrap overflow-hidden p-10 h-full">
-          <div
-            className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
-            style={{ left: '50%' }}
-          />
-          {MY_ABOUT.map((info, idx) => (
-            <div>
-              {(idx + 1) % 2 !== 0 ? (
-                <div className="mb-8 flex justify-between items-center w-full md:right-timeline">
-                  <div className="order-1 w-5/12"></div>
-                  <div className="invisible md:visible md:z-10 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                    <h1 className="mx-auto font-semibold text-lg text-white">
-                      {idx + 1}
-                    </h1>
-                  </div>
-                  <div className="order-1 bg-gray-200 rounded-lg shadow-xl md:w-5/12 z-10 px-6 py-4">
-                    <h3 className="mb-3 font-bold text-gray-800 text-xl">
-                      {info.title}
-                    </h3>
-                    <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
-                      {info.sub_title}
-                    </p>
-                  </div>
-                </div>
-              ) : (
-                <div className="mb-8 flex justify-between flex-row-reverse items-center w-full md:left-timeline">
-                  <div className="order-1 w-5/12"></div>
-                  <div className="invisible md:visible md:z-10 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                    <h1 className="mx-auto text-white font-semibold text-lg">
-                      {idx + 1}
-                    </h1>
-                  </div>
-                  <div className="order-1 bg-violet-700 rounded-lg shadow-xl md:w-5/12 z-10 px-6 py-4">
-                    <h3 className="mb-3 font-bold text-white text-xl">
-                      {info.title}
-                    </h3>
-                    <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-                      {info.sub_title}
-                    </p>
-                  </div>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
+        <MyTimeline />
       </div>
     </Element>
   )
