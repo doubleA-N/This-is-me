d82a3bbf30ea5fd965873542693519bb62f0c4d8
Aey-Thidarat <thidarat.c@pomelofashion.com>
2023-08-20 23:06:28 +0700

    feat(about-section): implement about section with static info

---
+++ src/app/components/about-section/const.js
@@ -0,0 +1,20 @@
+export const MY_ABOUT = [
+  {
+    title: 'My Graduation',
+    sub_title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  },
+  {
+    title: 'Internship',
+    sub_title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  },
+  {
+    title: 'Pomelo - Associate Software Engineer',
+    sub_title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  },
+  {
+    title: 'Pomelo - Senior Software Engineer',
+    sub_title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  },
+]
+
+export default { MY_ABOUT }
